3247d01194814422a4d0a1bbc71ab5c2d6f9650f
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-06-05 11:18:04 +0200

    Time left

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import {FormattedMessage } from "react-intl";
 
 export const Start = () => {
 
@@ -9,30 +10,39 @@ export const Start = () => {
   let [hours, setHours] = useState(0)
   let [minutes, setMinutes] = useState(0)
   let [seconds, setSeconds] = useState(0)
-  let now = new Date()
 
   useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date()
+      const timeDiff = dateEnd - now;
+      if (timeDiff > 0) {
 
-    setYear(dateEnd.getFullYear() - now.getFullYear())
-    setMonth(dateEnd.getMonth() - now.getMonth())
-    setDay(dateEnd.getDate() - now.getDate())
+        let hour = dateEnd.getHours() + 24 - now.getHours()
+        let minute = dateEnd.getMinutes() + 60 - now.getMinutes()
+        let second = dateEnd.getSeconds() + 60 - now.getSeconds()
 
-    setHours(dateEnd.getHours()+24 - now.getHours())
-    
-    setMinutes(dateEnd.getMinutes()+60 - now.getMinutes())
-    
-    setSeconds(dateEnd.getSeconds()+60 - now.getSeconds())
-    
-  },[])
+        setYear(dateEnd.getFullYear() - now.getFullYear())
+        setMonth(dateEnd.getMonth() - now.getMonth())
+        setDay(dateEnd.getDate() - now.getDate())
+        setHours(hour === 60 ? 0 : hour)
+        setMinutes(minute === 60 ? 0 : minute)
+        setSeconds(second === 60 ? 0 : second)
 
-    return (
-        <div>
-                <p>{year != 0 && year + " year "}</p>
-                <p>{month != 0 && month + " month "}</p>
-                <p>{day != 0 && day + (day === 1 ? " day": " days ")}</p>
-                <p>{hours} horas</p>
-                <p>{minutes} minutes</p>
-                <p>{seconds} seconds</p>
-        </div>
-    )
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000)
+
+  }, [])
+
+  return (
+    <div className="time">
+      {year !== 0 && <p>{year} <FormattedMessage id="year" /></p>}
+      {month !== 0 && <p>{month} <FormattedMessage id="month" /></p>}
+      {day !== 0 && <p>{day} {day === 1 ? <FormattedMessage id="day" /> : <FormattedMessage id="days" />}</p>}
+      <p>{hours} {hours === 1 ? <FormattedMessage id="hour" /> : <FormattedMessage id="hours" />}</p>
+      <p>{minutes} {minutes === 1 ? <FormattedMessage id="minute" /> : <FormattedMessage id="minutes" />}</p>
+      <p>{seconds} {seconds === 1 ? <FormattedMessage id="second" /> : <FormattedMessage id="seconds" />}</p>
+    </div>
+  )
 }
